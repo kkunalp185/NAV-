28e991e7ba1ee989f1e64368a56a7791043f75c7
anuj1963 <anujagrawal756@gmail.com>
2024-09-27 16:06:25 +0530

    Update date_filtered_nav_dashboard.py

--- date_filtered_nav_dashboard.py
+++ date_filtered_nav_dashboard.py
@@ -248,7 +248,7 @@ def main():
     # Trigger modification of the Excel file as soon as a selection is made
     if selected_workbook and selected_range:
         modify_and_push_to_github(selected_workbook)
-        st.experimental_rerun()  # Refresh the app after modification
+        
 
     if selected_workbook:
         st.write(f"### Displaying data from {selected_workbook}")
