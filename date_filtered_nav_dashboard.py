fa3e3ed261031ef276c42c7d33a9d7ca12196a3d
anuj1963 <anujagrawal756@gmail.com>
2024-09-30 11:55:24 +0530

    Update date_filtered_nav_dashboard.py

--- date_filtered_nav_dashboard.py
+++ date_filtered_nav_dashboard.py
@@ -103,8 +103,6 @@ def modify_workbook(filename):
             last_date_cell = ws.cell(row=ws.max_row, column=1).value
             if isinstance(last_date_cell, datetime):
                 last_date = last_date_cell
-            else:
-                last_date = datetime.now() - timedelta(days=30)
             next_date = last_date + timedelta(days=1)
 
             # Step 2: Identify the last non-zero NAV in column J (NAV)
